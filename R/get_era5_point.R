a2b5acf3e73740f9186ccb63b53473ca45effb50
Tadhg Moore <tadhg.moore6@gmail.com>
2023-09-21 16:52:57 +1200

    Added new fun

---
+++ R/get_era5_point.R
@@ -0,0 +1,146 @@
+#' Extract data from ERA5 RDS and format for `LakeEnsemblR```
+#'
+#' Extract met data frame from closest grid to lat & lon
+#'
+#' @param lat numeric; Latitude
+#' @param lon numeric; Longitude
+#' @param years numeric; vector of years in numeric form to be extracted
+#' @param variables vector; with lookup table for variable names
+#' @param format string; the format for column headers. Can be "aeme" or "ler".
+#' Defaults to "aeme".
+#'
+#' @importFrom sf st_as_sf
+#' @importFrom stars st_extract
+#' @importFrom parallel detectCores makeCluster stopCluster clusterExport
+#' parLapply
+#' @importFrom dplyr filter mutate across
+
+get_era5_point <- function(lat, lon, years, variables, format = "aeme",
+                           parallel = FALSE) {
+
+  token_file <- system.file("extdata/token.rds", package = "aemetools")
+  dtoken <- readRDS(token_file)
+  db_path <- "nz_era5_daily_rds/"
+
+  data("era5_ref_table", package = "AEME")
+
+    sel_vars <- era5_ref_table |>
+    dplyr::filter(aeme %in% variables)
+
+
+  if (parallel & length(variables) > 1) {
+    ncores <- min(c(parallel::detectCores() - 1, length(variables)))
+    cl <- parallel::makeCluster(ncores)
+    on.exit({
+      parallel::stopCluster(cl)
+    })
+    parallel::clusterExport(cl, varlist = list("years", "lat", "lon", "db_path",
+                                               "dtoken", "download_era5_point"),
+                            envir = environment())
+    message("Downloading ERA5 variables in parallel... ",
+            paste0("[", Sys.time(), "]"))
+    out <- parallel::parLapply(cl = cl, sel_vars$era5, function(v) {
+      download_era5_point(years = years, lat = lat, lon = lon, variable = v,
+                          db_path = db_path, dtoken = dtoken)
+    })
+  } else {
+    message("Downloading ERA5 variables... ",
+            paste0("[", Sys.time(), "] (Have you tried parallelising?)"))
+
+    out <- lapply(sel_vars$era5, function(v) {
+      download_era5_point(years = years, lat = lat, lon = lon, variable = v,
+                          db_path = db_path, dtoken = dtoken)
+    })
+  }
+
+  message("Finished downloading ERA5 variables! ", paste0("[", Sys.time(), "]"))
+
+  df <- Reduce(merge, out)
+
+  if("t2m" %in% colnames(df) | "d2m" %in% colnames(df)) {
+    sel_cols <- which(colnames(df) %in% c("t2m", "d2m"))
+    df[, sel_cols] <- df[, sel_cols] - 273.15
+  }
+  if("tp" %in% colnames(df) | "sf" %in% colnames(df)) {
+    sel_cols <- which(colnames(df) %in% c("tp", "sf"))
+    df[, sel_cols] <- df[, sel_cols] * 1000 # Convert to m
+  }
+
+  names(df) <- era5_ref_table[[format]][match(names(df), era5_ref_table$nc)]
+
+
+  df <- df |>
+    dplyr::mutate(dplyr::across(!contains("date"), signif))
+
+  na_rows <- which(!complete.cases(df))
+  if(length(na_rows) > 0) {
+    message("Removing NA's on ", paste0(df[na_rows, 1], collapse = ", "))
+    df <- na.exclude(df)
+  }
+  df
+}
+
+#' Download ERA5 data for a variable
+#'
+#' @inheritParams get_era5_point
+#'
+#' @importFrom rdrop2 drop_exists drop_download
+#' @importFrom stars st_extract
+#' @importFrom dplyr select
+#'
+#' @noRd
+
+download_era5_point <- function(years, lat, lon, variable, db_path, dtoken) {
+
+  out <- lapply(years, function(y) {
+    db1 <- paste0(db_path, "nz_era5-land_", y, "_", variable, "_1234_daily.rds")
+    db2 <- paste0(db_path, "nz_era5-land_", y, "_", variable, "_5678_daily.rds")
+    db3 <- paste0(db_path, "nz_era5-land_", y, "_", variable,
+                  "_9101112_daily.rds")
+
+    chk1 <- rdrop2::drop_exists(path = db1, dtoken = dtoken)
+    chk2 <- rdrop2::drop_exists(path = db2, dtoken = dtoken)
+    chk3 <- rdrop2::drop_exists(path = db3, dtoken = dtoken)
+
+    if (!any(c(chk1, chk2, chk3))) {
+      stop(strwrap("Files for selected years are not present currently in
+                     dropbox. Current available years are 1999-2021."))
+    }
+
+    f1 <- tempfile()
+    rdrop2::drop_download(path = db1, local_path = f1, dtoken = dtoken,
+                          progress = FALSE, verbose = FALSE)
+    f2 <- tempfile()
+    rdrop2::drop_download(path = db2, local_path = f2, dtoken = dtoken,
+                          progress = FALSE, verbose = FALSE)
+    f3 <- tempfile()
+    rdrop2::drop_download(path = db3, local_path = f3, dtoken = dtoken,
+                          progress = FALSE, verbose = FALSE)
+
+    if (!all(file.exists(c(f1, f2, f3)))) {
+      stop(strwrap("Files:\n'", f1, "'\n'", f2, "'\n'", f3, "'\n do not exist.
+                     Download has failed. Check your internet connection."))
+    }
+
+    coords <- data.frame(lat = lat, lon = lon)
+
+    coords_sf <- sf::st_as_sf(coords, coords = c("lon", "lat"), crs = 4326)
+
+    d1 <- readRDS(f1) |>
+      stars::st_extract(coords_sf) |>
+      as.data.frame() |>
+      dplyr::select(-geometry)
+    d2 <- readRDS(f2) |>
+      stars::st_extract(coords_sf) |>
+      as.data.frame() |>
+      dplyr::select(-geometry)
+    d3 <- readRDS(f3) |>
+      stars::st_extract(coords_sf) |>
+      as.data.frame() |>
+      dplyr::select(-geometry)
+
+    rbind(d1, d2, d3)
+  })
+
+  do.call(rbind, out)
+}
---
+++ tests/testthat/test-era5-download.R
@@ -0,0 +1,13 @@
+test_that("can download ERA5 point data", {
+
+  lon <- 176.2717
+  lat <- -38.079
+  data("era5_ref_table", package = "AEME")
+  variables <- c("MET_tmpair", "MET_tmpdew", "MET_wnduvu")
+
+  met <- get_era5_point(lat = lat, lon = lon, years = 2000,
+                        variables = variables, format = "aeme", parallel = TRUE)
+
+  testthat::expect_true(is.data.frame(met))
+  testthat::expect_true(ncol(met) == 4)
+})
