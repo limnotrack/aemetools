46d6428812f5c2fa25a20e98ba0bcf58029f5a93
Tadhg Moore <tadhg.moore6@gmail.com>
2023-12-04 15:09:44 +1300

    Add correction if pnt is outside ERA5 grid

--- tests/testthat/test-era5-download.R
+++ tests/testthat/test-era5-download.R
@@ -11,3 +11,19 @@ test_that("can download ERA5 point data", {
   testthat::expect_true(is.data.frame(met))
   testthat::expect_true(ncol(met) == 4)
 })
+
+test_that("can download ERA5 point data outside of grid", {
+
+  lon <- 179
+  lat <- -38.079
+  data("era5_ref_table", package = "AEME")
+  variables <- c("MET_tmpair")
+
+  met <- get_era5_point(lat = lat, lon = lon, years = 2000,
+                        variables = variables, format = "aeme", parallel = FALSE)
+
+  testthat::expect_true(is.data.frame(met))
+  testthat::expect_true(ncol(met) == 2)
+  testthat::expect_true(all(!is.na(met[, 2])))
+})
+
