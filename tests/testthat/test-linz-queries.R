94b39a1921f7fae473a14a73e31198f0d6a1c3d1
Tadhg Moore <tadhg.moore6@gmail.com>
2023-12-14 11:17:22 +1300

    Add get raster tile function

--- tests/testthat/test-linz-queries.R
+++ tests/testthat/test-linz-queries.R
@@ -14,8 +14,26 @@ test_that("can get layer value", {
   lon <- 175.337788
   lat <- -37.860736
 
-  elev <- get_raster_layer_value(lat = lat, lon = lon, layer = 104772)
+  elev <- get_raster_layer_value(lat = lat, lon = lon, layer_id = 104772)
 
   testthat::expect_equal(elev, 52.280998)
 
 })
+
+test_that("can get raster tile", {
+
+  lon <- 175.337788
+  lat <- -37.860736
+
+  # Make an sf object
+  shape <- sf::st_point(x = c(lon, lat), dim = "XY") |>
+    sf::st_sfc(crs = 4326) |>
+    sf::st_as_sf()
+
+  ras <- get_raster_tile(shape = shape, layer_id = 104772)
+
+  testthat::expect_true(is(ras, "SpatRaster"))
+  testthat::expect_equal(mean(terra::values(ras), na.rm = T), 35.456204)
+
+})
+
