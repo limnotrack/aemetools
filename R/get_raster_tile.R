5c6af96f26f1b95da5e7062890df55b2e65fc291
Tadhg Moore <tadhg.moore6@gmail.com>
2023-12-14 11:17:22 +1300

    Add get raster tile function

---
+++ R/get_raster_tile.R
@@ -0,0 +1,50 @@
+#' Get a raster tile from LINZ
+#'
+#' @param shape sf object with a single point or polygon.
+#' @inheritParams get_raster_layer_value
+#' @param zoom numeric; zoom level for the tile. Default is 15.
+#' @param key character; LINZ API key. If NULL, will look for the key in the
+#'  LINZ_KEY environment variable. If that is not set, will throw an error.
+#'  Use the \code{add_linz_key} function to set the key.
+#'
+#'  @importFrom maptiles create_provider get_tiles
+#'  @importFrom terra subset
+#'
+#' @return a raster object
+#' @export
+#'
+
+get_raster_tile <- function(shape, layer_id, zoom = 15, key = NULL,
+                            verbose = FALSE) {
+
+
+  if (is.null(key)) {
+    key <- Sys.getenv("LINZ_KEY")
+    if (key == "") {
+      stop("No LINZ API key found. See ?add_linz_key for more information.")
+    }
+  }
+
+  prov <- maptiles::create_provider(name = "LINZ",
+                                    url = paste0("https://tiles-cdn.koordinates.com/services;key=",
+                                                 key, "/tiles/v4/layer=", layer_id,
+                                                 "/EPSG:3857/{z}/{x}/{y}.png"),
+                                    citation = "LINZ")
+  catch <- tryCatch({
+    tiles <- maptiles::get_tiles(shape, provider = prov, zoom = zoom,
+                                 verbose = verbose)
+    TRUE
+  }, error = function(e) {
+    FALSE
+  })
+
+  if (!catch) {
+    return(invisible(NULL))
+  }
+
+  # ras <- raster::raster(tiles)
+  ras <- terra::subset(tiles, 1)
+  # raster::plot(ras)
+  # terra::plot(ras)
+  return(ras)
+}
