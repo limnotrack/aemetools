cfa313f4674c21643965fc33869d71f2d957ea6b
Tadhg Moore <tadhg.moore6@gmail.com>
2023-12-14 15:18:11 +1300

    New fun for getting sf objects & tests

--- tests/testthat/test-linz-queries.R
+++ tests/testthat/test-linz-queries.R
@@ -20,20 +20,59 @@ test_that("can get layer value", {
 
 })
 
-test_that("can get raster tile", {
+test_that("can get DEM raster tile", {
 
-  lon <- 175.337788
-  lat <- -37.860736
+  coords <-  nz_dem_metadata[2, ] |>
+    sf::st_transform(crs = 4326) |>
+    sf::st_centroid() |>
+    sf::st_coordinates() |>
+    as.numeric()
+  lon <- coords[1]
+  lat <- coords[2]
 
   # Make an sf object
   shape <- sf::st_point(x = c(lon, lat), dim = "XY") |>
     sf::st_sfc(crs = 4326) |>
     sf::st_as_sf()
 
-  ras <- get_raster_tile(shape = shape, layer_id = 104772)
+  ras <- get_raster_tile(shape = shape, layer_id = nz_dem_metadata$layer_id[2])
 
   testthat::expect_true(is(ras, "SpatRaster"))
-  testthat::expect_equal(mean(terra::values(ras), na.rm = T), 35.456204)
+  testthat::expect_equal(mean(terra::values(ras), na.rm = T), 19.592725)
+
+})
+
+test_that("can get aerial image raster tile", {
+
+  coords <- nz_aerial_imagery_metadata[1, ] |>
+    sf::st_transform(crs = 4326) |>
+    sf::st_centroid() |>
+    sf::st_coordinates() |>
+    as.numeric()
+  lon <- coords[1]
+  lat <- coords[2]
+
+  # Make an sf object
+  shape <- sf::st_point(x = c(lon, lat), dim = "XY") |>
+    sf::st_sfc(crs = 4326) |>
+    sf::st_as_sf()
+
+  ras <- get_raster_tile(shape = shape,
+                         layer_id = nz_aerial_imagery_metadata$layer_id[1])
+
+  testthat::expect_true(is(ras, "SpatRaster"))
+  testthat::expect_equal(mean(terra::values(ras), na.rm = T), 71.196337)
+
+})
+
+
+test_that("can get LINZ sf object", {
+
+  reg_council <- get_linz_sf(url = "https://datafinder.stats.govt.nz/",
+                             layer_id = 111182)
+
+  testthat::expect_true(is(reg_council, "sf"))
+  testthat::expect_equal(reg_council$C2023_V[1], "Northland Region")
 
 })
 
