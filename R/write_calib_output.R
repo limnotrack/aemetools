f9aca53100ffbd2523b0f623e7508d19b1b969ea
Tadhg Moore <tadhg.moore6@gmail.com>
2024-01-05 13:56:59 +1300

    Add file

---
+++ R/write_calib_output.R
@@ -0,0 +1,33 @@
+#' Write calibration output to file
+#'
+#' @inheritParams utils::write.csv
+#'
+#' @importFrom DBI dbConnect dbDisconnect dbWriteTable
+#' @importFrom duckdb duckdb
+#'
+#' @return \code{write_calib_output} writes the calibration output to a file
+#' @noRd
+#'
+
+write_calib_output <- function(x, file) {
+  type <- tools::file_ext(file)
+  gen_n <- x[1, "gen"]
+
+  messae("Writing output for generation ", gen_n, " to ", file, ". [",
+         Sys.time(), "]")
+
+  if (type == "csv") {
+    if (gen_n == 1) {
+      write.csv(x, file, quote = TRUE, row.names = FALSE)
+    } else {
+      write.table(x, file, append = TRUE, sep = ",", row.names = FALSE,
+                  col.names = FALSE)
+    }
+  } else if (type == "db") {
+    con <- DBI::dbConnect(duckdb::duckdb(), dbdir = file)
+    append <- gen_n > 1
+    DBI::dbWriteTable(con, "calib_output", as.data.frame(x),
+                      overwrite = !append, append = append)
+    DBI::dbDisconnect(con, shutdown = TRUE)
+  }
+}
