b51c7d6235cbab62a55e5e7358b98e7346162234
Tadhg Moore <tadhg.moore6@gmail.com>
2024-05-16 18:26:32 +0000

    Updated to work with params

--- R/update_param.R
+++ R/update_param.R
@@ -1,16 +1,25 @@
 #' Update parameter values in param based on best_pars
 #'
+#' @inheritParams get_param
 #' @param param A data frame with parameters to update
-#' @param best_pars A data frame with the best parameters from `get_param`
+#' @param best_pars A data frame with the best parameters from `get_param`.
+#' Defaults to NULL. When NULL, `get_param` is called to get the best
+#' parameters.
 #'
 #' @return data frame with updated parameter values for running the model with
 #'  `run_aeme_param`
 #' @export
 
-update_param <- function(param, best_pars) {
+update_param <- function(param, calib, na_value,
+                         fit_col = "fit", best_pars = NULL) {
+
+  if (is.null(best_pars)) {
+    best_pars <- get_param(calib = calib, na_value = na_value,
+                           fit_col = fit_col, best = TRUE)
+  }
 
   for (i in seq_len(nrow(best_pars))) {
-    idx <- grepl(best_pars$param2[i], param$name) & grepl(best_pars$model[i],
+    idx <- grepl(best_pars$name[i], param$name) & grepl(best_pars$model[i],
                                                           param$model)
     if (sum(idx) == 0)
       stop("Parameter ", best_pars$param2[i], " not found in param")
@@ -18,7 +27,7 @@ update_param <- function(param, best_pars) {
     if (sum(idx) > 1)
       stop("Parameter ", best_pars$param2[i], " found in multiple places for ",
                            best_pars$model[i])
-    param[idx, "value"] <- best_pars$value[i]
+    param[idx, "value"] <- best_pars$parameter_value[i]
   }
   param
 }
