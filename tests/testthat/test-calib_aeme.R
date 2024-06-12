561499083cf7955c67c1caa764b11c08770ce748
Tadhg Moore <tadhg.moore6@gmail.com>
2024-06-13 11:52:20 +1200

    Add test for GLM

--- tests/testthat/test-calib_aeme.R
+++ tests/testthat/test-calib_aeme.R
@@ -627,6 +627,84 @@ test_that("can calibrate lake level w/ scaling outflow and level from wbal only 
 
 })
 
+test_that("can calibrate lake level w/ scaling outflow and level from wbal only for AEME-GLM in parallel", {
+  tmpdir <- tempdir()
+  aeme_dir <- system.file("extdata/lake/", package = "AEME")
+  # Copy files from package into tempdir
+  file.copy(aeme_dir, tmpdir, recursive = TRUE)
+  path <- file.path(tmpdir, "lake")
+  aeme <- AEME::yaml_to_aeme(path = path, "aeme.yaml")
+  obs <- AEME::observations(aeme)
+  obs$lake <- NULL
+  obs$level <- NULL
+  AEME::observations(aeme) <- obs
+  model_controls <- AEME::get_model_controls()
+  inf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  outf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  model <- c("glm_aed", "gotm_wet")
+  aeme <- AEME::build_aeme(path = path, aeme = aeme,
+                           model = model, model_controls = model_controls,
+                           inf_factor = inf_factor, ext_elev = 5,
+                           use_bgc = FALSE)
+  aeme <- AEME::run_aeme(aeme = aeme, model = model,
+                         verbose = FALSE, path = path)
+  # AEME::plot(aeme, model = model, path = path, plot = "calib",
+  #            obs = "temp", save = FALSE, show = FALSE)
+  lke <- AEME::lake(aeme)
+  file_chk <- file.exists(file.path(path, paste0(lke$id, "_",
+                                                 tolower(lke$name)),
+                                    model, "output", "output.nc"))
+  testthat::expect_true(file_chk)
+
+  utils::data("aeme_parameters", package = "aemetools")
+  param <- aeme_parameters |>
+    dplyr::filter(model %in% model & grepl("wdr|inf", file)) |>
+    dplyr::mutate(min = 0, max = 2.5)
+
+  # Function to calculate fitness
+  fit <- function(df) {
+    O <- df$obs
+    P <- df$model
+    mean(abs(O - P))
+    # -1 * (cor(x = O, y = P, method = "pearson") -
+    #         (mean(abs(O - P)) / (max(O) - min(O))))
+  }
+  FUN_list <- list(HYD_temp = fit, LKE_lvlwtr = fit)
+
+  ctrl <- create_control(method = "calib", VTR = -Inf, NP = 10, itermax = 30,
+                         reltol = 0.07, cutoff = 0.25, mutate = 0.1,
+                         parallel = TRUE, file_type = "csv",
+                         na_value = 999, ncore = 2L)
+
+  vars_sim <- c("LKE_lvlwtr")
+  weights <- c("LKE_lvlwtr" = 1)
+
+  fit <- run_and_fit(aeme = aeme, param = param,
+                     model = model, path = path, FUN_list = FUN_list,
+                     vars_sim = vars_sim, weights = weights,
+                     return_indices = FALSE,
+                     include_wlev = TRUE,
+                     fit = TRUE)
+
+  testthat::expect_true(fit$LKE_lvlwtr < 0.25)
+
+  # Calibrate AEME model
+  sim_id <- calib_aeme(aeme = aeme, path = path,
+                       param = param, model = model,
+                       FUN_list = FUN_list, ctrl = ctrl,
+                       vars_sim = vars_sim, weights = weights)
+
+  calib <- read_calib(ctrl = ctrl, sim_id = sim_id)
+
+  testthat::expect_true(is.list(calib))
+
+  plist <- plot_calib(calib = calib, na_value = ctrl$na_value)
+  testthat::expect_true(is.list(plist))
+
+  testthat::expect_true(all(sapply(plist, ggplot2::is.ggplot)))
+
+})
+
 test_that("can calibrate lake level w/ scaling outflow only for AEME-GOTM in parallel", {
   tmpdir <- tempdir()
   aeme_dir <- system.file("extdata/lake/", package = "AEME")
