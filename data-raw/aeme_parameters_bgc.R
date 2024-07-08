4a0dc9cdf36942149a35297169dc4daf79eb9188
Tadhg Moore <tadhg.moore6@gmail.com>
2024-07-08 16:32:17 +1200

    Move parameters to AEME

--- data-raw/aeme_parameters_bgc.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `aeme_parameters_bgc` dataset goes here
-
-aeme_parameters_bgc <- read.csv("data-raw/aeme_parameters_bgc.csv") |>
-  dplyr::mutate(group = NA)
-
-usethis::use_data(aeme_parameters_bgc, overwrite = TRUE)
