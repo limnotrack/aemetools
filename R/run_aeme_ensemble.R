1edebdfd3807e5ade450576cdc9d1a750915838b
Tadhg Moore <tadhg.moore6@gmail.com>
2024-07-12 04:09:04 +0000

    Updated function to be standalone & use pars slot

--- R/run_aeme_ensemble.R
+++ R/run_aeme_ensemble.R
@@ -8,25 +8,34 @@
 #' @export
 #'
 
-run_aeme_ensemble <- function(aeme, sim_id, param, calib = NULL, path = ".",
-                              fit_col = "fit", ctrl) {
+run_aeme_ensemble <- function(aeme, model, n = 10, dist = "norm", path = ".",
+                              parallel = FALSE, ncores = NULL, param = NULL,
+                              na_value = 999) {
 
   # Check inputs
   if (!is(aeme, "aeme")) stop("aeme must be an aeme object")
-  if (!is.character(sim_id)) stop("sim_id must be a character")
-  if (!is.data.frame(param)) stop("param must be a data frame")
+  # if (!is.character(sim_id)) stop("sim_id must be a character")
+  # if (!is.data.frame(param)) stop("param must be a data frame")
 
-  if (is.null(calib)) {
-    calib <- read_calib(ctrl = ctrl, sim_id = sim_id)
+  # if (is.null(calib)) {
+  #   calib <- read_calib(ctrl = ctrl, sim_id = sim_id)
+  # }
+
+  if (is.null(param)) {
+    param <- AEME::parameters(aeme)
   }
 
-  model <- names(sim_id)
+  # model <- names(sim_id)
   cfg <- AEME::configuration(aeme)
   model_controls <- cfg$model_controls
   outp <- AEME::output(aeme)
-  lke <- AEME::lake(aeme)
-  lakename <- tolower(lke[["name"]])
   lake_dir <- AEME::get_lake_dir(aeme = aeme, path = path)
+
+  if (dist == "norm") {
+    FUN <- rnorm_limits
+  } else if (dist == "unif") {
+    FUN <- runif
+  }
 
   mod_list <- lapply(model, \(m) {
   # Catch if calib is NULL
@@ -34,64 +43,70 @@ run_aeme_ensemble <- function(aeme, sim_id, param, calib = NULL, path = ".",
     model_pars <- param |>
       dplyr::filter(model == m)
 
-    # Identify the fixed and variable columns
-    fixed_cols <- c("gen", "run", "fit_value")
+    new_params <- sapply(seq_len(nrow(model_pars)), \(i) {
+      FUN(n = n, min = model_pars$min[i], max = model_pars$max[i])
+    }) |>
+      as.data.frame()
+    names(new_params) <- paste0(model_pars$group, "/", model_pars$name)
 
-    # Select parameters below the cutoff
-    sid <- sim_id[m]
-    sel_pars <- calib$simulation_data |>
-      dplyr::filter(sim_id == sid & fit_type == fit_col) |>
-      tidyr::pivot_wider(id_cols = dplyr::all_of(fixed_cols),
-                         names_from = parameter_name,
-                         values_from = parameter_value)
-    variable_cols <- setdiff(names(sel_pars), fixed_cols)
-
-    # Filter out duplicates based on the variable columns
-    filtered_sel_pars <- sel_pars |>
-      dplyr::distinct(dplyr::across(dplyr::all_of(variable_cols)),
-                      .keep_all = TRUE)
-
-    print(filtered_sel_pars)
-
-    cutoff_value <- quantile(filtered_sel_pars$fit_value, ctrl$cutoff)
-    # filt_pars <- filtered_sel_pars |>
-    #   dplyr::filter(fit_value <= cutoff_value) |>
+    # # Identify the fixed and variable columns
+    # fixed_cols <- c("gen", "run", "fit_value")
+    #
+    # # Select parameters below the cutoff
+    # sid <- sim_id[m]
+    # sel_pars <- calib$simulation_data |>
+    #   dplyr::filter(sim_id == sid & fit_type == fit_col) |>
+    #   tidyr::pivot_wider(id_cols = dplyr::all_of(fixed_cols),
+    #                      names_from = parameter_name,
+    #                      values_from = parameter_value)
+    # variable_cols <- setdiff(names(sel_pars), fixed_cols)
+    #
+    # # Filter out duplicates based on the variable columns
+    # filtered_sel_pars <- sel_pars |>
+    #   dplyr::distinct(dplyr::across(dplyr::all_of(variable_cols)),
+    #                   .keep_all = TRUE)
+    #
+    # print(filtered_sel_pars)
+    #
+    # cutoff_value <- quantile(filtered_sel_pars$fit_value, ctrl$cutoff)
+    # # filt_pars <- filtered_sel_pars |>
+    # #   dplyr::filter(fit_value <= cutoff_value) |>
+    # #   dplyr::rename(fit = fit_value)
+    # # ctrl <- list(na_value = 999,)
+    # param_df <- filtered_sel_pars |>
+    #   dplyr::select(-gen, - run) |>
     #   dplyr::rename(fit = fit_value)
-    # ctrl <- list(na_value = 999,)
-    param_df <- filtered_sel_pars |>
-      dplyr::select(-gen, - run) |>
-      dplyr::rename(fit = fit_value)
+    #
+    # new_params <- next_gen_params(param_df = param_df, param = model_pars,
+    #                               ctrl = ctrl, add_mutation = FALSE,
+    #                               keep_best_pars = TRUE)
 
-    new_params <- next_gen_params(param_df = param_df, param = model_pars,
-                                  ctrl = ctrl, add_mutation = FALSE,
-                                  keep_best_pars = TRUE)
-
-    if (is.null(ctrl$ncore)) {
-      ctrl$ncore <- parallel::detectCores() - 1
-      if (ctrl$ncore > nrow(new_params)) ctrl$ncore <- nrow(new_params)
+    if (is.null(ncores)) {
+      ncores <- parallel::detectCores() - 1
+      if (ncores > nrow(new_params)) ncores <- nrow(new_params)
     }
 
-    # Correct N of splits if ncore is greater than number of parameters
-    splts <- min(ctrl$NP, ctrl$ncore)
+    # Correct N of splits if ncores is greater than number of parameters
+    splts <- ncores # min(ctrl$NP, ncores)
 
     suppressWarnings({
       param_list <- split(new_params, 1:splts)
     })
 
     # Loop through each of the parameters
-    if (ctrl$parallel) {
+    if (parallel) {
 
-      temp_dirs <- make_temp_dir(m, lake_dir, n = ctrl$ncore)
+      temp_dirs <- make_temp_dir(m, lake_dir, n = ncores)
       # list.files(temp_dirs[1], recursive = TRUE)
-      ncores <- min((parallel::detectCores() - 1), ctrl$ncore, ctrl$NP)
+      tryCatch(parallel::stopCluster(cl), error = function(e) {})
       cl <- parallel::makeCluster(ncores)
       on.exit(parallel::stopCluster(cl))
       varlist <- list("param_list", "aeme", "path", "m", "model_pars",
-                      "temp_dirs","ctrl")
+                      "temp_dirs")
       parallel::clusterExport(cl, varlist = varlist,
                               envir = environment())
-      message("Running an ensemble of ", m, " with ", ctrl$NP,
-              " members using ", ctrl$ncore, " cores. ", "[",
+      message("Running an ensemble of ", m, " with ", n,
+              " members using ", ncores, " cores. ", "[",
               format(Sys.time()), "]")
 
       # Run the ensemble in parallel
@@ -113,7 +128,7 @@ run_aeme_ensemble <- function(aeme, sim_id, param, calib = NULL, path = ".",
           # message(i, ", ", p)
 
           a2 <- aemetools::run_aeme_param(aeme = aeme, param = model_pars, model = m,
-                                          path = path, na_value = ctrl$na_value,
+                                          path = path, na_value = na_value,
                                           return_aeme = TRUE)
           outp2 <- AEME::output(a2)
           outp2[["ens_001"]][[m]]
@@ -137,7 +152,7 @@ run_aeme_ensemble <- function(aeme, sim_id, param, calib = NULL, path = ".",
         # message(i, ", ", p)
 
         a2 <- aemetools::run_aeme_param(aeme = aeme, param = model_pars, model = m,
-                                        path = path, na_value = ctrl$na_value,
+                                        path = path, na_value = na_value,
                                         return_aeme = TRUE)
         outp2 <- AEME::output(a2)
         outp2[["ens_001"]][[m]]
@@ -150,7 +165,7 @@ run_aeme_ensemble <- function(aeme, sim_id, param, calib = NULL, path = ".",
   # Add output to the AEME object
   ens_n <- outp$n_members
   outp <- list()
-  for (i in 1:ctrl$NP) {
+  for (i in 1:n) {
     ens_n <- i # ens_n + 1
     ens_lab <- paste0("ens_", sprintf("%03d", ens_n))
     outp[[ens_lab]] <- list(dy_cd = mod_list[["dy_cd"]][[i]],
@@ -161,4 +176,14 @@ run_aeme_ensemble <- function(aeme, sim_id, param, calib = NULL, path = ".",
 
   AEME::output(aeme) <- outp
   return(aeme)
+}
+
+#' Generate a normal distribution with a min and a max
+#'
+#' @noRd
+#'
+rnorm_limits <- function(n, min, max) {
+  x <- rnorm(n)
+  x <- (max - min) * x/diff(range(x))
+  return(x - min(x) + min)
 }
