b11b1539af8503898b11f59cff09ad5b579cfbf9
Tadhg Moore <tadhg.moore6@gmail.com>
2024-07-24 12:25:06 +1200

    Add ability to update parameters in the aeme obj

--- R/update_param.R
+++ R/update_param.R
@@ -1,23 +1,34 @@
 #' Update parameter values in param based on best_pars
 #'
+#' @inheritParams AEME::build_aeme
 #' @inheritParams get_param
 #' @param param A data frame with parameters to update. Defaults to NULL. When
 #' NULL, the parameter values are extracted from `calib$parameter_metadata`.
 #' @param best_pars A data frame with the best parameters from `get_param`.
 #' Defaults to NULL. When NULL, `get_param` is called to get the best
 #' parameters.
+#' @param na_value The value to replace NA values with. Defaults to NULL. When
+#' NULL, the value is extracted from `calib$calibration_metadata$na_value`.
+#' @param aeme aeme; object. Defaults to NULL. When NULL, a dataframe of the
+#' updated parameter values is returned. When provided, the updated parameter
+#' values are added to the aeme object and the aeme object is returned.
+#' @param quantile The quantile to use for the top quantile of the fit_value.
+#' Defaults to 0.1.
 #'
 #' @return data frame with updated parameter values for running the model with
 #'  `run_aeme_param`
 #' @export
 
-update_param <- function(calib, param = NULL, na_value, prob = 0.1,
-                         fit_col = "fit", best_pars = NULL) {
+update_param <- function(calib, param = NULL, na_value = NULL, aeme = NULL,
+                         quantile = 0.1, fit_col = "fit", best_pars = NULL) {
 
   if (is.null(param)) {
     param_column_names <- get_param_column_names()
     param <- calib$parameter_metadata |>
       dplyr::select(all_of(param_column_names))
+  }
+  if (is.null(na_value)) {
+    na_value <- calib$calibration_metadata$na_value[1]
   }
   if (is.null(best_pars)) {
     best_pars <- get_param(calib = calib, na_value = na_value,
@@ -26,11 +37,12 @@ update_param <- function(calib, param = NULL, na_value, prob = 0.1,
   pars <- get_param(calib = calib, na_value = na_value,
                     fit_col = fit_col, best = FALSE)
 
-  # Calculate min/max for each sim_id and parameter for the top prob % of the fit_value
+  # Calculate min/max for each sim_id and parameter for the top quantile of the
+  # fit_value
   min_max <- pars |>
     dplyr::filter(fit_value != na_value) |>
     dplyr::group_by(sim_id, name) |>
-    dplyr::filter(fit_value <= quantile(fit_value, prob)) |>
+    dplyr::filter(fit_value <= quantile(fit_value, quantile)) |>
     dplyr::summarise(min = min(parameter_value),
                      max = max(parameter_value), .groups = "drop")
 
@@ -52,7 +64,16 @@ update_param <- function(calib, param = NULL, na_value, prob = 0.1,
   param <- param |>
     dplyr::select(dplyr::all_of(calib_col_names))
 
-  return(param)
+  if (is.null(aeme)) {
+    return(param)
+  } else {
+    old_pars <- AEME::parameters(aeme)
+    if (nrow(old_pars) > 0) {
+      param <- dplyr::bind_rows(old_pars, param)
+    }
+    AEME::parameters(aeme) <- param
+    return(aeme)
+  }
 }
 
 #' Get the column names for the calibration data frame
