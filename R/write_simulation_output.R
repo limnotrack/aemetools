fcb8d568b00fe5f74af050acf69e632b77d693dc
tadhg-moore <tadhg.moore6@gmail.com>
2025-11-13 21:50:35 +0000

    Handle missing index col

--- R/write_simulation_output.R
+++ R/write_simulation_output.R
@@ -14,16 +14,16 @@
 
 write_simulation_output <- function(x, ctrl, aeme, model, param, FUN_list,
                                     sim_id = NULL, append_metadata = TRUE) {
-
+  
   # Extract meta information
   lke <- AEME::lake(aeme)
   tme <- AEME::time(aeme)
   cfg <- AEME::configuration(aeme)
   use_bgc <- cfg$use_bgc
-
+  
   # Create function string
   fun_string <- lapply(FUN_list, deparse1)
-
+  
   # Check if ctrl exists & assign sim id
   type <- ctrl$file_type
   iter <- 1
@@ -33,7 +33,7 @@ write_simulation_output <- function(x, ctrl, aeme, model, param, FUN_list,
   } else {
     sim_stem <- paste0(strsplit(sim_id, "_")[[1]][1:3], collapse = "_")
   }
-
+  
   # Table names
   tbl_names <- c("lake_metadata", "simulation_metadata", "function_metadata",
                  "parameter_metadata", "simulation_data")
@@ -41,11 +41,11 @@ write_simulation_output <- function(x, ctrl, aeme, model, param, FUN_list,
   if (!dir.exists(path)) {
     dir.create(path, recursive = TRUE)
   }
-
+  
   file_to_check <- ifelse(type == "db", ctrl$file_name,
                           "simulation_metadata.csv")
   file_to_check <- file.path(path, file_to_check)
-
+  
   sim_stem_chk <- data.frame()
   add_lake_meta <- TRUE
   if (file.exists(file_to_check)) {
@@ -71,7 +71,7 @@ write_simulation_output <- function(x, ctrl, aeme, model, param, FUN_list,
         dplyr::pull(id)
       add_lake_meta <- !lke$id %in% lke_ids
     }
-
+    
     if (nrow(sim_stem_chk) > 0) {
       prev_iter <- sim_stem_chk |>
         dplyr::slice_tail(n = 1) |>
@@ -81,37 +81,39 @@ write_simulation_output <- function(x, ctrl, aeme, model, param, FUN_list,
       iter <- prev_iter + 1
     }
   }
-
+  
   if (is.null(sim_id)) {
     # format to a 3 character number, pad with leading zeros
     sim_id <- paste0(sim_stem, "_", formatC(iter, width = 3, flag = "0"))
   }
-
+  
   if (append_metadata) {
     # Convert lke list to dataframe
-    lake_meta <- data.frame(name = lke$name, id = lke$id, latitude = lke$latitude,
-                            longitude = lke$longitude, elevation = lke$elevation,
+    lake_meta <- data.frame(name = lke$name, id = lke$id,
+                            latitude = lke$latitude,
+                            longitude = lke$longitude, 
+                            elevation = lke$elevation,
                             depth = lke$depth, area = lke$area)
-
+    
     # Simulation metadata
     sim_meta <- data.frame(sim_id = sim_id, id = lke$id, model = model,
                            spin_up = tme$spin_up[[model]], start = tme$start,
                            stop = tme$stop, use_bgc = use_bgc,
                            n_params = nrow(param), method = ctrl$method,
                            time_started = format(Sys.time()))
-
+    
     # Function metadata
     fun_meta <- lapply(names(FUN_list), \(f) {
       data.frame(sim_id = sim_id, var = f, fun = deparse1(FUN_list[[f]]))
     }) |>
       dplyr::bind_rows()
-
+    
     # Extract parameter info
-    param_column_names <- get_param_column_names()
+    param_column_names <- AEME::param_colnames(incl_opt = FALSE)
     param_meta <- param |>
       dplyr::mutate(sim_id = sim_id, group = as.character(group)) |>
       dplyr::select(sim_id, dplyr::all_of(param_column_names))
-
+    
     # Sensitivity analysis metadata
     if (ctrl$method == "sa") {
       sa_meta <- lapply(names(ctrl$vars_sim), \(n) {
@@ -124,18 +126,18 @@ write_simulation_output <- function(x, ctrl, aeme, model, param, FUN_list,
         dplyr::bind_rows()
     }
   }
-
-
+  
+  
   # Format simulation output
   sims <- x |>
     as.data.frame() |>
     dplyr::mutate(run = 1:dplyr::n()) |>
     dplyr::select(run, dplyr::everything())
-
+  
   if (!"gen" %in% names(sims)) {
     sims$gen <- 1
   }
-
+  
   # pivot_longer to get the output in long format
   sim_data <- sims |>
     tidyr::pivot_longer(cols = dplyr::contains("/"), names_to = "pname",
@@ -145,29 +147,31 @@ write_simulation_output <- function(x, ctrl, aeme, model, param, FUN_list,
     as.data.frame() |>
     dplyr::rename(parameter_name = pname, parameter_value = pvalue) |>
     dplyr::mutate(sim_id = sim_id) |>
-    dplyr::select(sim_id, gen, run, parameter_name, parameter_value, fit_type, fit_value)
-
-
+    dplyr::select(sim_id, gen, run, parameter_name, parameter_value, fit_type, 
+                  fit_value)
+  
+  
   if (!append_metadata) {
     output <- list(simulation_data = sim_data)
   } else {
     output <- list(lake_metadata = lake_meta, simulation_metadata = sim_meta,
-                   function_metadata = fun_meta, parameter_metadata = param_meta,
+                   function_metadata = fun_meta,
+                   parameter_metadata = param_meta,
                    simulation_data = sim_data)
     if (ctrl$method == "sa") {
       output$sensitivity_metadata <- sa_meta
     }
   }
-
+  
   if ("gen" %in% colnames(sim_data)) {
     gen_n <- sim_data[1, "gen"]
-
+    
     file_to_print <- ifelse(type == "db", ctrl$file_name, "simulation_data.csv")
-
+    
     message("Writing output for generation ", gen_n, " to ", file_to_print,
             " with sim ID: ", sim_id, " [", format(Sys.time()), "]")
   }
-
+  
   if (type == "csv") {
     write_to_csv(output = output, sim_id = sim_id, gen_n = gen_n,
                  add_lake_meta = add_lake_meta, path = path)
@@ -201,6 +205,25 @@ write_to_csv <- function(output, path, sim_id, gen_n, add_lake_meta = FALSE) {
     if (tbl_name == "lake_metadata" && !add_lake_meta) next
     
     df <- output[[i]]
+    
+    # Handle parameter_metadata index column
+    if (tbl_name == "parameter_metadata" && file_chk) {
+      existing_header <- names(read.csv(fname, nrows = 1, check.names = FALSE))
+      
+      # Add missing index column if missing
+      if (!"index" %in% existing_header) {
+        message("Adding missing column 'index' to 'parameter_metadata.csv'...")
+        # Append column to existing CSV file
+        temp_file <- tempfile(fileext = ".csv")
+        # Read full file, add column, rewrite
+        dat <- read.csv(fname, check.names = FALSE)
+        dat$index <- NA
+        write.csv(dat, temp_file, row.names = FALSE, quote = TRUE)
+        # Replace old file with new one
+        file.copy(temp_file, fname, overwrite = TRUE)
+        unlink(temp_file)
+      }
+    }
     
     # --- Handle sensitivity_metadata specifically ---
     if (tbl_name == "sensitivity_metadata" && file_chk) {
@@ -279,6 +302,21 @@ write_to_db <- function(file, path, output, add_lake_meta = FALSE) {
     if (tbl_name == "parameter_metadata" && "group" %in% names(df)) {
       df$group[is.na(df$group)] <- "NA"
     }
+    
+    if (tbl_name == "parameter_metadata") {
+      # Add index column if missing
+      if (file_chk && tbl_name %in% DBI::dbListTables(con)) {
+        existing_cols <- DBI::dbListFields(con, tbl_name)
+        if (!"index" %in% existing_cols) {
+          message("Adding missing column 'index' (INTEGER) to 'parameter_metadata'...")
+          DBI::dbExecute(
+            con,
+            "ALTER TABLE parameter_metadata ADD COLUMN index INTEGER;"
+          )
+        }
+      }
+    }
+    
     
     # --- Strictly handle sensitivity_metadata schema ---
     if (tbl_name == "sensitivity_metadata" &&
