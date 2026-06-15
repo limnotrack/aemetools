69f03a52a80dc53f4b1f79ef408ec1c2433ec9a9
Tadhg Moore <tadhg.moore6@gmail.com>
2026-06-16 08:36:20 +1200

    remove bug

--- R/plot_ensemble.R
+++ R/plot_ensemble.R
@@ -25,11 +25,6 @@ plot_ensemble <- function(aeme, model, var_sim = "HYD_temp", depth = NULL,
   # Check inputs
   aeme <- AEME::check_aeme(aeme)
   model <- AEME::check_model(model)
-  if (missing(path)) {
-    path <- AEME::get_aeme_path(aeme)
-  } else {
-    path <- AEME::check_path(path)
-  }
 
   # Check if var_sim is a character vector
   if (!is.character(var_sim)) {
