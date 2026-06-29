ecd90b189a31d18aff6e3f3ee14cf234756ed298
Tadhg Moore <tadhg.moore6@gmail.com>
2026-06-29 14:28:41 +1200

    delete ltapi funs

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  ns <- asNamespace(pkgname)
-  if (exists(".lt_schema_cache", envir = ns)) {
-    unlockBinding(".lt_schema_cache", ns)
-  }
-  assign(".lt_schema_cache", new.env(parent = emptyenv()), envir = ns)
-}
